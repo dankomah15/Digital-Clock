f34bd1235a8539207f63fb5c7b297a5d6ca97194
Ankomah Daniel <[ankomahdaniel995@gmail.com]>
2025-05-28 21:34:08 +0000

    my first commit

--- script.js
+++ script.js
@@ -21,7 +21,8 @@ function updateClock(){
     hourEl.innerText = h;
     minuteEl.innerText = m;
     secondEl.innerText = s;
-    ampmEl,(innerText=ampm);
+    ampmEl.innerText = ampm;
+
     setTimeout(() =>{
         updateClock();
 
